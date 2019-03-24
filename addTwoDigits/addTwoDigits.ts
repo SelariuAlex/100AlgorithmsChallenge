00b4078d8ee0571ffb930dc6d5ec8fc27b2753c5
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-03-24 21:28:00 +0200

    addDigits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,14 @@
-function addTwoDigits(n: any): number {
+function addTwoDigits(n) {
+  let splitNr = n.toString().split('');
+  if (splitNr.length > 1) {
+    let result = splitNr.reduce((add, e) => Number(e) + add, 0);
 
+    console.log(result);
+  } else {
+    console.log(n);
+  }
 }
 
-// console.log(addTwoDigits(29));
+addTwoDigits(29);
+addTwoDigits(3);
+addTwoDigits(295);

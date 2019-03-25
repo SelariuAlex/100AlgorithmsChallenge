c9e72e3fa151e41bd6442697ee874422d7279785
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-03-25 09:13:01 +0200

    adjacentElemet

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,12 @@
-function addBorder(picture: string[]): string[] {
-
+function addBorder(picture) {
+  let margin = '*****';
+  let border = [...picture].map(e => `*${e}*`);
+  border.unshift(margin);
+  border.push(margin);
+  return border;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(['abc', 'ded']));
+
+console.log(addBorder(['abc', 'ded', 'fdg']));
+console.log(addBorder(['abc', 'ded', 'zcz', 'sadasfgafda']));
--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,11 @@
-function adjacentElementsProduct(inputArray: number[]): number {
+function adjacentElementsProduct(inputArray) {
+  let bigNr = inputArray[0] * inputArray[1];
 
+  for (let i = 1; i < inputArray.length - 1; i++) {
+    let currentNr = inputArray[i] * inputArray[i + 1];
+    bigNr = bigNr < currentNr ? currentNr : bigNr;
+  }
+  return bigNr;
 }
 
 console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));

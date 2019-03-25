e691b99be4f6d553b4389dfde892f0457ec399c6
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-03-25 11:30:31 +0200

    offSequence

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,20 @@
-function almostIncreasingSequence(sequence: number[]): boolean {
+function almostIncreasingSequence(sequence) {
+  let offSequence = 0;
+  for (let i = 0; i < sequence.length; i++) {
+    if (sequence[i] <= sequence[i - 1]) {
+      offSequence++;
+      if (
+        sequence[i] <= sequence[i - 2] &&
+        sequence[i + 1] <= sequence[i - 1]
+      ) {
+        return false;
+      }
+    }
+  }
 
+  return offSequence <= 1;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1]));
+console.log(almostIncreasingSequence([1, 3, 2]));
+console.log(almostIncreasingSequence([1, 2, 4, 5, 2, 3, 6]));

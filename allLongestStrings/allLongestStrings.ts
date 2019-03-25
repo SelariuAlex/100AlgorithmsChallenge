9a0da64009d1c712210f9a67f3fb7aceecc71482
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-03-25 09:46:30 +0200

    longestString

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,8 @@
 function allLongestStrings(inputArray: string[]): string[] {
-
+  let arrLeng = inputArray.map(e => e.length);
+  let maxLeng = Math.max(...arrLeng);
+  let longest = inputArray.filter(e => e.length === maxLeng);
+  return longest;
 }
 
-console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(['aba', 'aa', 'ad', 'vcd', 'aba']));

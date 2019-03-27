2d0b9c1474db8ac380f1dbf24b785180dccb52f9
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-03-27 18:24:20 +0200

    alphabetSubSeq

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,51 @@
-function alphabetSubsequence(s: string): boolean {
+function alphabetSubsequence(s) {
+  let alphabet = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
+  let stringArr = s.split('');
+  let result = null;
+  let curent = -1;
 
+  for (let i = 0; i < stringArr.length; i++) {
+    if (curent < alphabet.indexOf(stringArr[i])) {
+      curent = alphabet.indexOf(stringArr[i]);
+      result = true;
+    } else {
+      result = false;
+      return result;
+    }
+  }
+
+  return result;
 }
 
-console.log(alphabetSubsequence('zab'))
-console.log(alphabetSubsequence('effg'))
-console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
-console.log(alphabetSubsequence('bxz'))
+console.log(alphabetSubsequence('zab'));
+console.log(alphabetSubsequence('effg'));
+console.log(alphabetSubsequence('cdce'));
+console.log(alphabetSubsequence('ace'));
+console.log(alphabetSubsequence('bxz'));

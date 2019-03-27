b8b05783e335e372a15a6b50afde7462f1f1450e
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-03-27 17:47:58 +0200

    alphabetShift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,44 @@
-function alphabeticShift(inputString: string): string {
+function alphabeticShift(inputString) {
+  let alphabet = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
+  let nextLetterArr = [];
+  let strArr = inputString.split('');
 
+  for (let i = 0; i < strArr.length; i++) {
+    let nextIndex = alphabet.indexOf(strArr[i]) + 1;
+    if (nextIndex >= alphabet.length) {
+      nextIndex = 0;
+    }
+    nextLetterArr.push(alphabet[nextIndex]);
+  }
+
+  return nextLetterArr.join('');
 }
 
 console.log(alphabeticShift('crazy'));

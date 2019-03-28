ca56eecef8147fc568dc51b9e1c1939cfb0fb0e9
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-03-28 10:59:15 +0200

    areSimilar

--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -1,7 +1,28 @@
-function areSimilar(a: number[], b: number[]): boolean {
-   
+function areSimilar(a, b) {
+  let firstArr = [];
+  let secondArr = [];
+
+  if (a.toString() === b.toString()) {
+    return true;
+  }
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      firstArr.push(a[i]);
+      secondArr.push(b[i]);
+    }
+  }
+
+  secondArr = secondArr.reverse();
+  if (firstArr.length === 2 && firstArr.toString() === secondArr.toString()) {
+    return true;
+  }
+
+  return false;
 }
 
 console.log(areSimilar([1, 2, 3], [1, 2, 3]));
 console.log(areSimilar([1, 2, 3], [2, 1, 3]));
 console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(areSimilar([1, 2, 3], [3, 1, 2]));
+console.log(areSimilar([1, 2, 3, 4], [2, 1, 3, 4]));

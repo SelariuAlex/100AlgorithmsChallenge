5757c188712eb411278021a8b388ed3f28269aea
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-03-28 10:36:55 +0200

    strongArm

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,13 @@
-function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+function areEquallyStrong(yourLeft, yourRight, friendsLeft, friendsRight) {
+  let yourWeakest = yourLeft <= yourRight ? yourLeft : yourRight;
+  let yourStrongest = yourLeft >= yourRight ? yourLeft : yourRight;
+  let friendWeakest = friendsLeft <= friendsRight ? friendsLeft : friendsRight;
+  let friendStrongest =
+    friendsLeft >= friendsRight ? friendsLeft : friendsRight;
 
+  return yourStrongest === friendStrongest && yourWeakest === friendWeakest;
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 9));

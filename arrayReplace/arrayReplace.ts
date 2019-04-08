6c73b729d50636b22d1e21541d3813a046986c56
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-04-08 15:48:44 +0300

    arrayReplace

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,12 @@
-function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-   
+function arrayReplace(inputArray, elemToReplace, substitutionElem) {
+  let newArr = inputArray.map(e => {
+    if (elemToReplace === e) {
+      e = substitutionElem;
+    }
+    return e;
+  });
+  return newArr;
 }
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
+console.log(arrayReplace([1, 2, 3, 4, 2, 2], 2, 9));

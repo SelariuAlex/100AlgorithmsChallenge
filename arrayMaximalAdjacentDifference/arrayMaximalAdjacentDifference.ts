d2a77bd2c65c75b5c937409f25bc58805a5fa84d
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-04-09 19:31:09 +0300

    arrayMaximalAdjacentDifference

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,13 @@
-function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+function arrayMaximalAdjacentDifference(inputArray) {
+  let maxDif = Math.abs(inputArray[0] - inputArray[1]);
 
+  for (let i = 0; i < inputArray.length; i++) {
+    let absoluteDif = Math.abs(inputArray[i - 1] - inputArray[i]);
+
+    maxDif = absoluteDif > maxDif ? absoluteDif : maxDif;
+  }
+
+  return maxDif;
 }
 
 console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));

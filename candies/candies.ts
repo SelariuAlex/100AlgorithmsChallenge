2add1b46c1e995cfc850d5c99fd3470a0d291811
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-04-10 19:06:00 +0300

    candies

--- candies/candies.ts
+++ candies/candies.ts
@@ -1,5 +1,8 @@
-function candies(n: number, m: number): number {
-
+function candies(children, candys) {
+  let nrOfcandys = Math.floor(candys / children);
+  return nrOfcandys;
 }
 
 console.log(candies(3, 10));
+console.log(candies(2, 10));
+console.log(candies(4, 10));

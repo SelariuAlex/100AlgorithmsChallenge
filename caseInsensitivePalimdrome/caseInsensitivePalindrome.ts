a55a93fe5afafcbf87518294778fb3352f8e61af
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-04-11 17:06:02 +0300

    palindrome

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,18 @@
-function isCaseInsensitivePalindrome(inputString: string): boolean {
+function isCaseInsensitivePalindrome(inputString) {
+  inputString = inputString.toLowerCase();
+  let inputArr = inputString
+    .split('')
+    .reverse()
+    .join('');
+  let reversStr = inputArr;
 
+  if (inputString === reversStr) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 console.log(isCaseInsensitivePalindrome('AaBaa'));
 console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome('racecar'));

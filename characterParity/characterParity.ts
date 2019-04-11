ef42845bdbbf538c4aa5fdc186f437cb776c0277
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-04-11 17:34:55 +0300

    characterParity

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,7 +1,16 @@
-function characterParity(symbol: string): string {
-   
+function characterParity(symbol) {
+  let convert = parseInt(symbol);
+  if (isNaN(convert)) {
+    return 'not a digit';
+  } else if (convert % 2 === 0) {
+    return 'even';
+  } else {
+    return 'odd';
+  }
 }
 
-console.log(characterParity('5'))
-console.log(characterParity('8'))
-console.log(characterParity('q'))
+console.log(characterParity('5'));
+console.log(characterParity('8'));
+console.log(characterParity('q'));
+console.log(characterParity('25'));
+console.log(characterParity(null));

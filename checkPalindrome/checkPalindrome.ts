bfa4c21f7a92a2962002fe0b9ba61a9377329338
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-04-12 19:18:24 +0300

    made palindrome work with space and '

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,7 +1,20 @@
-function checkPalindrome(inputString: string): boolean {
+function checkPalindrome(string) {
+  string = string.toLowerCase();
+  let characterArr = string.split('');
+  let validCharacters = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  let letterArr = [];
+  characterArr.forEach(element => {
+    if (validCharacters.indexOf(element) > -1) letterArr.push(element);
+  });
 
+  if (letterArr.join('') === letterArr.reverse().join('')) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 console.log(checkPalindrome('aabaa'));
 console.log(checkPalindrome('abac'));
 console.log(checkPalindrome('a'));
+console.log(checkPalindrome("Madam I'm Adam"));

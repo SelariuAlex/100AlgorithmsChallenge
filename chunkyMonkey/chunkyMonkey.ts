49c90765c5269c54a853a82c7fe8d95656a70c48
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-04-14 14:39:37 +0300

    chunkyMonkey

--- chunkyMonkey/chunkyMonkey.ts
+++ chunkyMonkey/chunkyMonkey.ts
@@ -1,6 +1,13 @@
-function chunkyMonkey(arr: any[], size: number): any[][] {
+function chunkyMonkey(arr, size) {
+  let result = [];
+  let chunks = Math.ceil(arr.length / size);
 
+  for (let i = 0; i < chunks; i++) {
+    result.push(arr.splice(0, size));
+  }
+  return result;
 }
 
-console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
+console.log(chunkyMonkey(['a', 'b', 'c', 'd'], 2));
 console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
+console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 1));

70412db33b368311952f31cdaa0c947d4fcc2d01
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-04-16 21:11:18 +0300

    containsDuplicats works for strings to

--- containsDuplicates/containsDuplicates.ts
+++ containsDuplicates/containsDuplicates.ts
@@ -1,6 +1,13 @@
-function containsDuplicates(a: number[]): boolean {
-
+function containsDuplicates(a) {
+  for (let i = 0; i < a.length; i++) {
+    if (a.lastIndexOf(a[i]) !== i) {
+      return false;
+    }
+  }
+  return true;
 }
 
 console.log(containsDuplicates([1, 2, 3, 1]));
 console.log(containsDuplicates([3, 1]));
+console.log(containsDuplicates('abadc'));
+console.log(containsDuplicates('abdc'));

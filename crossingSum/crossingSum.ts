84c9b8623091c7cc536980b2cf68927b03fa933e
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-04-19 11:32:21 +0300

    crossingSum

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,7 +1,17 @@
-function crossingSum(matrix: number[][], a: number, b: number): number {
-  
+function crossingSum(matrix, a, b) {
+  let columTotal = matrix[a].reduce((acc, curr) => acc + curr);
+  let rowTotal = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    rowTotal += i !== a ? matrix[i][b] : 0;
+  }
+  return columTotal + rowTotal;
 }
 
-console.log(crossingSum([[1, 1, 1, 1], 
-    [2, 2, 2, 2], 
-    [3, 3, 3, 3]], 1, 3));
+console.log(crossingSum([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]], 1, 3));
+
+console.log(crossingSum([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]], 2, 3));
+
+console.log(
+  crossingSum([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]], 3, 1)
+);

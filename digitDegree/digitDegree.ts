178f2d566fad09fb93d6d3b38ee7ebb13f2ff9f2
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-04-23 18:00:56 +0300

    digitDegree

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,7 +1,21 @@
-function digitDegree(n: number): number {
-  
+function digitDegree(n) {
+  let nrOfReplaces = 0;
+
+  function sumOfNumbers(nr) {
+    nr = nr.toString().split('');
+    n = nr.reduce((a, v) => parseInt(a) + parseInt(v));
+  }
+
+  while (n > 9) {
+    sumOfNumbers(n);
+    nrOfReplaces++;
+  }
+
+  return nrOfReplaces;
 }
 
 console.log(digitDegree(5));
+console.log(digitDegree(9));
 console.log(digitDegree(10));
 console.log(digitDegree(91));
+console.log(digitDegree(81));

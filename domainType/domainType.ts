9563f15773b346b6d26bc5bff7695770a1d8e1fe
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-04-26 17:19:18 +0300

    domain types

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,33 @@
-function domainType(domains: string[]): string[] {
+function domainType(domains) {
+  let domainType = [];
 
+  for (let i = 0; i < domains.length; i++) {
+    let domain = domains[i].split('.');
+    let lastDomain = domain[domain.length - 1];
+
+    if (lastDomain === 'org') {
+      domainType.push('organization');
+    } else if (lastDomain === 'com') {
+      domainType.push('commercial');
+    } else if (lastDomain === 'net') {
+      domainType.push('network');
+    } else if (lastDomain === 'info') {
+      domainType.push('information');
+    }
+  }
+  return domainType;
 }
 
-console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
+console.log(
+  domainType(['en.wiki.org', 'codefights.com', 'happy.net', 'code.info'])
+);
+
+console.log(
+  domainType([
+    'en.wiki.org',
+    'org.com',
+    'codefights.com',
+    'happy.net',
+    'code.info'
+  ])
+);

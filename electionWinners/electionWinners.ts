9dfcc41f688ae2f3088700c35d506b73fa9621cf
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-04-26 17:52:49 +0300

    election Winners

--- electionWinners/electionWinners.ts
+++ electionWinners/electionWinners.ts
@@ -1,5 +1,11 @@
-function electionsWinners(votes: number[], k: number): number {
-
+function electionsWinners(votes, k) {
+  let first = Math.max(...votes);
+  let winners = votes.filter(e => {
+    return e + k > first;
+  });
+  return winners.length;
 }
 
 console.log(electionsWinners([2, 3, 5, 2], 3));
+console.log(electionsWinners([2, 3, 5, 6], 4));
+console.log(electionsWinners([2, 3, 5, 2], 1));

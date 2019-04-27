f91e9f5fa98208d6980ca1f81decdc118e9b0f9f
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-04-27 12:17:47 +0300

    factorialize with recursion

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,6 +1,12 @@
-function factorializeANumber(num: number): number {
-
+function factorializeANumber(num) {
+  if (num < 0) return -1;
+  else if (num === 0) return 1;
+  else {
+    return num * factorializeANumber(num - 1);
+  }
 }
 
 console.log(factorializeANumber(5));
 console.log(factorializeANumber(10));
+console.log(factorializeANumber(2));
+console.log(factorializeANumber(1));

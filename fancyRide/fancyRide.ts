992c46cc35dba87366067b1cc87e80c70a33906b
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-02 20:53:42 +0300

    fancyRide

--- fancyRide/fancyRide.ts
+++ fancyRide/fancyRide.ts
@@ -1,5 +1,17 @@
-function fancyRide(l: number, fares: number[]): string {
+function fancyRide(leng, fares) {
+  let rides = ['UberX', 'UberXL', 'UberPlus', 'UberBlack', 'UberSUV'];
+  let answer = '';
 
+  for (let i = fares.length - 1; i >= 0; i--) {
+    if (fares[i] * leng <= 20) {
+      answer = rides[i];
+      break;
+    } else {
+      answer = "can't aford it";
+    }
+  }
+  return answer;
 }
 
 console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
+console.log(fancyRide(100, [0.3, 0.5, 0.7, 1, 1.3]));

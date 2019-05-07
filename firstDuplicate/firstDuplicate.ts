20aa93c2d0386d3e7883eec59616355b35958dc4
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-07 19:04:36 +0300

    first Duplicate

--- firstDuplicate/firstDuplicate.ts
+++ firstDuplicate/firstDuplicate.ts
@@ -1,6 +1,17 @@
-function firstDuplicate(a: number[]): number {
-    
+function firstDuplicate(a) {
+  const obj = {};
+
+  for (let i = 0; i < a.length; i++) {
+    if (!obj[a[i]]) {
+      obj[a[i]] = 'no duplicate';
+    } else {
+      return a[i];
+    }
+  }
+
+  return -1;
 }
 
 console.log(firstDuplicate([2, 1, 3, 5, 3, 2]));
 console.log(firstDuplicate([2, 4, 3, 5, 1]));
+console.log(firstDuplicate([2, 4, 3, 5, 1, 5, 4]));

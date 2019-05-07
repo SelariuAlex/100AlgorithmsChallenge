864fe9664320744d2f4a535c83fee86ba324ad8c
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-07 19:40:50 +0300

    first not repeating

--- firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
+++ firstNotRepeatingCharacter/firstNotRepeatingCharacter.ts
@@ -1,6 +1,23 @@
-function firstNotRepeatingCharacter(s: string): string {
+function firstNotRepeatingCharacter(s) {
+  const obj = {};
 
+  for (let i = 0; i < s.length; i++) {
+    if (!obj[s[i]]) {
+      obj[s[i]] = 1;
+    } else {
+      obj[s[i]]++;
+    }
+  }
+
+  for (let key in obj) {
+    if (obj[key] === 1) {
+      return key;
+    }
+  }
+
+  return '_';
 }
 
 console.log(firstNotRepeatingCharacter('abacabad'));
 console.log(firstNotRepeatingCharacter('abacabaabacaba'));
+console.log(firstNotRepeatingCharacter('bcbadef'));

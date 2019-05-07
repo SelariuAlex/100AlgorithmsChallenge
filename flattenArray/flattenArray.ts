88cc80b4303f9112cc892011c9136621c99d5c45
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-07 19:51:19 +0300

    flattenArray

--- flattenArray/flattenArray.ts
+++ flattenArray/flattenArray.ts
@@ -1,6 +1,8 @@
-function flattenArray(arr: any[]): any[] {
-
+function flattenArray(arr) {
+  let arrLeng = arr.length;
+  let flatArr = arr.flat(arrLeng);
+  return flatArr;
 }
 
-console.log(flattenArray([[["a"]], [["b"]]]));
+console.log(flattenArray([[['a']], [['b']]]));
 console.log(flattenArray([1, [2], [3, [[4]]]]));

348ae50076d3a4b595f4e3b11a386880ac73536e
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-08 19:19:14 +0300

    drowing plant

--- growingPlant/growingPlant.ts
+++ growingPlant/growingPlant.ts
@@ -1,5 +1,17 @@
-function growingPlant(upSpeed: number, downSpeed: number, desiredHeight: number): number {
+function growingPlant(upSpeed, downSpeed, desiredHeight) {
+  let totalHeight = 0;
+  let days = 0;
 
+  while (totalHeight < desiredHeight) {
+    days++;
+    totalHeight += upSpeed;
+
+    if (totalHeight < desiredHeight) {
+      totalHeight -= downSpeed;
+    }
+  }
+
+  return days;
 }
 
-console.log(growingPlant(100, 10, 910))
+console.log(growingPlant(100, 10, 910));

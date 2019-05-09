e8816e32fb0fbe13f25318fd869bc52023012e4d
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-09 17:18:18 +0300

    house sumn

--- houseNumbersSum/houseNumbersSum.ts
+++ houseNumbersSum/houseNumbersSum.ts
@@ -1,5 +1,12 @@
-function houseNumbersSum(inputArray: number[]): number {
-
+function houseNumbersSum(inputArray) {
+  let houses = 0;
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] === 0) {
+      return houses;
+    }
+    houses += inputArray[i];
+  }
 }
 
 console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
+console.log(houseNumbersSum([0, 1, 2, 3, 0, 1, 5, 0, 2]));

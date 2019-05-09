3c34b4ecedb4c89ec46b52df1cb0491597add5ad
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-09 17:34:13 +0300

    house of cats

--- houseOfCats/houseOfCats.ts
+++ houseOfCats/houseOfCats.ts
@@ -1,6 +1,17 @@
-function houseOfCats(legs: number): number[] {
+function houseOfCats(legs) {
+  let cat = Math.floor(legs / 4);
+  let human = Math.floor(legs / 2);
+  let legArr = [];
 
+  if (!cat) {
+    legArr = [human];
+    return legArr;
+  } else {
+    legArr = [cat, human];
+    return legArr;
+  }
 }
 
 console.log(houseOfCats(6));
 console.log(houseOfCats(2));
+console.log(houseOfCats(4));

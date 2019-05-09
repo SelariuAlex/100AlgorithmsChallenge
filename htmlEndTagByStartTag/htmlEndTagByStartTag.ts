01a2564146d61fd1112c6ac69aad51216f175db5
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-09 18:10:43 +0300

    html end tag , using regex

--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,6 +1,10 @@
-function htmlEndTagByStartTag(startTag: string): string {
+function htmlEndTagByStartTag(startTag) {
+  let startTagArr = startTag.split(' ');
+  let tag = startTagArr[0];
+  tag = tag.replace(/\W/g, '');
 
+  return `</${tag}>`;
 }
 
 console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
-console.log(htmlEndTagByStartTag('<i>'))
+console.log(htmlEndTagByStartTag('<i>'));

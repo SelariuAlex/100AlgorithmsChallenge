0b5133a1446af78b85d56c3b6637f51187215d50
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-10 18:17:33 +0300

    iteger to string fixed width

--- integerToStringOfWixedWidth/integerToStringOfFixedWidth.ts
+++ integerToStringOfWixedWidth/integerToStringOfFixedWidth.ts
@@ -1,5 +1,20 @@
-function integerToStringOfFixedWidth(number: number, width: number): string {
-  
+function integerToStringOfFixedWidth(number, width) {
+  let stringNr = number.toString();
+  let nrWidth = stringNr.length;
+  let cutString = '';
+
+  if (width > nrWidth) {
+    return stringNr.padStart(width, '0');
+  }
+
+  if (nrWidth === width) {
+    return stringNr;
+  }
+
+  if (width < nrWidth) {
+    cutString = stringNr.substring(width, nrWidth);
+    return cutString;
+  }
 }
 
 console.log(integerToStringOfFixedWidth(1234, 2));

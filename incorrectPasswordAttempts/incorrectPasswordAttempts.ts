7d2f84fc87f95ddf0fcb190f1c85a76f2a9a9e3b
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-10 16:50:45 +0300

    incorrect pass

--- incorrectPasswordAttempts/incorrectPasswordAttempts.ts
+++ incorrectPasswordAttempts/incorrectPasswordAttempts.ts
@@ -1,5 +1,47 @@
-function incorrectPasscodeAttempts(passcode: string, attempts: string[]): boolean {
+function incorrectPasscodeAttempts(passcode, attempts) {
+  let attemptNr = 0;
+  for (let i = 0; i < attempts.length; i++) {
+    if (passcode === attempts[i]) {
+      attemptNr = 0;
+    } else {
+      attemptNr++;
+    }
 
+    if (attemptNr === 10) {
+      return true;
+    }
+  }
+  return false;
 }
 
-console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));
+console.log(
+  incorrectPasscodeAttempts('1111', [
+    '1111',
+    '4444',
+    '9999',
+    '3333',
+    '8888',
+    '2222',
+    '7777',
+    '0000',
+    '6666',
+    '7285',
+    '5555',
+    '1111'
+  ])
+);
+
+console.log(
+  incorrectPasscodeAttempts('0000', [
+    '1111',
+    '4444',
+    '9999',
+    '3333',
+    '8888',
+    '2222',
+    '7777',
+    '6666',
+    '1111',
+    '0000'
+  ])
+);

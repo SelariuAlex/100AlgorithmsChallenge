6d71581635f7870ca7a21404592358e257ab4528
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-11 09:29:23 +0300

    is lucky

--- isLucky/isLucky.ts
+++ isLucky/isLucky.ts
@@ -1,5 +1,18 @@
-function isLucky(n: number): boolean {
-    
+function isLucky(n) {
+  let stringNr = n.toString();
+  let half = stringNr.length / 2;
+
+  let firstHalf = sumNumString(stringNr.substring(0, half));
+  let secondHalf = sumNumString(stringNr.substring(half));
+
+  function sumNumString(nr) {
+    return nr
+      .split('')
+      .map(char => parseInt(char))
+      .reduce((num1, num2) => num1 + num2);
+  }
+
+  return firstHalf === secondHalf;
 }
 
 console.log(isLucky(1230));

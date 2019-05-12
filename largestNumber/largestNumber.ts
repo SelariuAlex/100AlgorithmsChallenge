5bfc6ca27b9a17dd288223b5439d5eee570553e6
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-12 12:58:21 +0300

    largest num

--- largestNumber/largestNumber.ts
+++ largestNumber/largestNumber.ts
@@ -1,5 +1,9 @@
-function largestNumber(n: number): number {
- 
+function largestNumber(n) {
+  let nr = '9'.repeat(n);
+
+  return parseInt(nr);
 }
 
 console.log(largestNumber(2));
+console.log(largestNumber(1));
+console.log(largestNumber(5));

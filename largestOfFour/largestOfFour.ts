12d6e0fab7f671a5065fdcd36df80122d271d91a
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-12 13:30:48 +0300

    largest 4

--- largestOfFour/largestOfFour.ts
+++ largestOfFour/largestOfFour.ts
@@ -1,6 +1,26 @@
-function largestOfFour(nums: number[][]): number[] {
+function largestOfFour(nums) {
+  let largestNr = [];
 
+  for (let i = 0; i < nums.length; i++) {
+    largestNr.push(Math.max(...nums[i]));
+  }
+
+  return largestNr;
 }
 
-console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) );
-console.log(largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]));
+console.log(
+  largestOfFour([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1]
+  ])
+);
+console.log(
+  largestOfFour([
+    [4, 9, 1, 3],
+    [13, 35, 18, 26],
+    [32, 35, 97, 39],
+    [1000000, 1001, 857, 1]
+  ])
+);

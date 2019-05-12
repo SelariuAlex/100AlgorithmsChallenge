376483b7841483b2b497ba286272a211a28277b4
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-12 12:48:35 +0300

    is tandem

--- isTandemRepeat/isTandemRepeat.ts
+++ isTandemRepeat/isTandemRepeat.ts
@@ -1,7 +1,17 @@
-function isTandemRepeat(inputString: string): boolean {
-  
+function isTandemRepeat(inputString) {
+  const wordLength = inputString.length;
+
+  if (wordLength % 2 === 0) {
+    let fistHalf = inputString.slice(0, wordLength / 2);
+    let secondHalf = inputString.slice(wordLength / 2, wordLength);
+
+    return fistHalf === secondHalf;
+  }
+
+  return false;
 }
 
-console.log(isTandemRepeat('tandemtandem'))
-console.log(isTandemRepeat('qqq'))
-console.log(isTandemRepeat('2w2ww'))
+console.log(isTandemRepeat('tandemtandem'));
+console.log(isTandemRepeat('qqq'));
+console.log(isTandemRepeat('2w2ww'));
+console.log(isTandemRepeat('2we2ww'));

5266d2bff68f686f33fdb7e6bf68485aabafa24d
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-13 17:33:16 +0300

    late ride

--- lateRide/lateRide.ts
+++ lateRide/lateRide.ts
@@ -1,5 +1,18 @@
-function lateRide(n: number): number {
+function lateRide(n) {
+  let ore = Math.floor(n / 60);
+  let min = n % 60;
 
+  let oreString = ore.toString();
+  let minString = min.toString();
+
+  let allDigits = oreString.concat(minString);
+
+  let sumOfDigits = allDigits
+    .split('')
+    .map(e => parseInt(e))
+    .reduce((a, b) => a + b);
+
+  return sumOfDigits;
 }
 
 console.log(lateRide(240));

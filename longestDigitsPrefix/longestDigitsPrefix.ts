5c94d049f55a02bc6da0b3e29f63bc2280a97024
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-14 18:11:17 +0300

    longest prefix

--- longestDigitsPrefix/longestDigitsPrefix.ts
+++ longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,5 +1,17 @@
-function longestDigitsPrefix(inputString: string): string {
+function longestDigitsPrefix(inputString) {
+  let stringArr = inputString.split('');
+  let prefix = '';
 
+  for (let e of stringArr) {
+    let num = parseInt(e);
+
+    if (isNaN(num)) {
+      break;
+    }
+
+    prefix += e;
+  }
+  return prefix;
 }
 
 console.log(longestDigitsPrefix('123aa1'));

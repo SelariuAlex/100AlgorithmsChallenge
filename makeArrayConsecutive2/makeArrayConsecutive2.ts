2c4cb3c0fd676a6475b5ef828cc132678a1b23ee
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-15 16:02:18 +0300

    makeArrayConsecutive

--- makeArrayConsecutive2/makeArrayConsecutive2.ts
+++ makeArrayConsecutive2/makeArrayConsecutive2.ts
@@ -1,5 +1,17 @@
-function makeArrayConsecutive2(statues: number[]): number {
+function makeArrayConsecutive2(statues) {
+  let neededStatues = 0;
+  statues = statues.sort((a, b) => a - b);
+  const min = statues[0];
+  const max = statues[statues.length - 1];
 
+  for (let i = min; i < max; i++) {
+    if (!statues.includes(i)) {
+      neededStatues++;
+    }
+  }
+
+  return neededStatues;
 }
 
 console.log(makeArrayConsecutive2([6, 2, 3, 8]));
+console.log(makeArrayConsecutive2([1, 2, 3, 14]));

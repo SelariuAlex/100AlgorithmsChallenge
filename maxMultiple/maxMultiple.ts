d82923c0fbf76c57dfcb17e5de52be4642ca5dbd
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-18 15:53:43 +0300

    max multiple

--- maxMultiple/maxMultiple.ts
+++ maxMultiple/maxMultiple.ts
@@ -1,5 +1,7 @@
-function maxMultiple(divisor: number, bound: number): number  {
+function maxMultiple(divisor, bound) {
+  let remainder = bound % divisor;
 
+  return bound - remainder;
 }
 
-console.log(maxMultiple(3,10));
+console.log(maxMultiple(3, 10));

b2f4159da3057fd636f3b8ea5992ddfa05b2e4d1
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-05-26 19:03:14 +0300

    missing letter

--- missingLetters/missingLetters.ts
+++ missingLetters/missingLetters.ts
@@ -1,7 +1,44 @@
-function missingLetters(str: string): string {
-   
+function missingLetters(str) {
+  const alphabet = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
+
+  if (str[0] !== 'a') {
+    return undefined;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== alphabet[i]) {
+      return alphabet[i];
+    }
+  }
 }
-console.log(missingLetters("bce"));
-console.log(missingLetters("abce"));
-console.log(missingLetters("abcdefghjklmno"));
-console.log(missingLetters("abcdefghijklmnopqrstuvwxyz"));
+console.log(missingLetters('bce'));
+console.log(missingLetters('abce'));
+console.log(missingLetters('abcdefghjklmno'));
+console.log(missingLetters('abcdefghijklmnopqrstuvwxyz'));

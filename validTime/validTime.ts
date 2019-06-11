cd7d4441b402c29d399d9bde205e92c6a44773a2
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-06-11 17:48:47 +0300

    valid time

--- validTime/validTime.ts
+++ validTime/validTime.ts
@@ -1,5 +1,23 @@
-function validTime(time: string): boolean {
+function validTime(time) {
+  // const OreMinute = time.split(':');
+  // if (OreMinute[0] > 23) {
+  //   return false;
+  // }
+  // if (OreMinute[1] > 59) {
+  //   return false;
+  // }
+  // return true;
 
+  const [ore, minute] = time.split(':');
+
+  if (parseInt(ore) > 23 || parseInt(ore) < 0) {
+    return false;
+  }
+  if (parseInt(minute) > 59 || parseInt(minute) < 0) {
+    return false;
+  }
+
+  return true;
 }
 
 console.log(validTime('13:58'));

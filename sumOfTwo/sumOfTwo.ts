e9ea3792b31103a1a97015a706a9b21fc6a9a6cf
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-06-12 17:11:15 +0300

    sum of two

--- sumOfTwo/sumOfTwo.ts
+++ sumOfTwo/sumOfTwo.ts
@@ -1,5 +1,19 @@
-function sumOfTwo(a: number[], b: number[], v: number): boolean {
+function sumOfTwo(a, b, v) {
+  const numbersNeeded = {};
 
+  for (let num of a) {
+    const difference = v - num;
+    numbersNeeded[difference] = difference;
+  }
+
+  for (let num of b) {
+    if (numbersNeeded.hasOwnProperty(num)) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42));
+console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 44));

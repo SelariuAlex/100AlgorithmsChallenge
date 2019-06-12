18c4c163d2412aaac430ea020366a0d20c5c76c5
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-06-12 15:48:41 +0300

    unique Digits products

--- uniqueDigitProducts/uniqueDigitsProducts.ts
+++ uniqueDigitProducts/uniqueDigitsProducts.ts
@@ -1,4 +1,26 @@
-function uniqueDigitProducts(a: number[]): number {
+function uniqueDigitProducts(numArr) {
+  const singleDigitNumbers = [];
+
+  numArr.forEach(num => {
+    const product = getSingleDigit(num);
+
+    if (!singleDigitNumbers.includes(product)) {
+      singleDigitNumbers.push(product);
+    }
+  });
+
+  return singleDigitNumbers.length;
+}
+
+function getSingleDigit(num) {
+  const digits = num.toString().split('');
+  let result = 1;
+
+  digits.forEach(digit => {
+    result *= parseInt(digit);
+  });
+
+  return result;
 }
 
 console.log(uniqueDigitProducts([2, 8, 121, 42, 222, 23]));

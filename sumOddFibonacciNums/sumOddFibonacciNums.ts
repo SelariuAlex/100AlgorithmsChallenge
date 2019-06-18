94d44058af4a8c81ecec533284d6eace2836d8df
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-06-18 12:15:07 +0300

    odd fibonacii

--- sumOddFibonacciNums/sumOddFibonacciNums.ts
+++ sumOddFibonacciNums/sumOddFibonacciNums.ts
@@ -1,5 +1,19 @@
-function sumOddFibonacciNums(num: number): number {
+function sumOddFibonacciNums(num) {
+  let result = 0;
+  let prevoius = 0;
+  let current = 1;
 
+  while (current <= num) {
+    if (current % 2 !== 0) {
+      result += current;
+    }
+
+    let nextCurrent = current + prevoius;
+    prevoius = current;
+    current = nextCurrent;
+  }
+
+  return result;
 }
 
 console.log(sumOddFibonacciNums(1000));

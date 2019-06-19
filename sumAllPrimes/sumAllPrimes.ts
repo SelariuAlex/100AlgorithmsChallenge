e0da0f24ee8238c777358769248239790620fb8a
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-06-19 14:39:06 +0300

    sum all primes

--- sumAllPrimes/sumAllPrimes.ts
+++ sumAllPrimes/sumAllPrimes.ts
@@ -1,5 +1,19 @@
-function sumAllPrimes(num: number): number {
+function sumAllPrimes(num) {
+  let store = [];
+  let primes = [];
 
+  for (let i = 2; i <= num; ++i) {
+    if (!store[i]) {
+      primes.push(i);
+      for (let j = i << 1; j <= num; j += i) {
+        store[j] = true;
+      }
+    }
+  }
+
+  return primes.reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+  );
 }
 
 console.log(sumAllPrimes(10));

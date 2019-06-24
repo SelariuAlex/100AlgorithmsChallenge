b55def30d429bcf19eda2ae4b3664178819cd5cc
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-06-24 17:19:11 +0300

    sort height

--- sortByHeight/sortByHeight.ts
+++ sortByHeight/sortByHeight.ts
@@ -1,5 +1,15 @@
-function sortByHeight(a: number[]): number[] {
+function sortByHeight(a) {
+  let sortedArr = a.filter(num => num !== -1).sort((num1, num2) => num1 - num2);
+  let indexVal = 0;
 
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== -1) {
+      a[i] = sortedArr[indexVal];
+      indexVal++;
+    }
+  }
+
+  return a;
 }
 
 console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));

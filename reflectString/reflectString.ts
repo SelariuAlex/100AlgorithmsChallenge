38479e6c0c2afd8e335285fd486bf74a9bbb334a
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-06-27 19:02:34 +0300

    reflectedString

--- reflectString/reflectString.ts
+++ reflectString/reflectString.ts
@@ -1,5 +1,39 @@
-function reflectString(inputString: string): string {
-  
+function reflectString(inputString) {
+  const alphabet = {
+    z: 'a',
+    y: 'b',
+    x: 'c',
+    w: 'd',
+    v: 'e',
+    u: 'f',
+    t: 'g',
+    s: 'h',
+    r: 'i',
+    q: 'j',
+    p: 'k',
+    o: 'l',
+    n: 'm',
+    m: 'n',
+    l: 'o',
+    k: 'p',
+    j: 'q',
+    i: 'r',
+    h: 's',
+    g: 't',
+    f: 'u',
+    e: 'v',
+    d: 'w',
+    c: 'x',
+    b: 'y',
+    a: 'z'
+  };
+  let stringArr = inputString.split('');
+
+  let reflectedWord = '';
+
+  stringArr.forEach(char => (reflectedWord += alphabet[char]));
+
+  return reflectedWord;
 }
 
-console.log(reflectString("name"));
+console.log(reflectString('name'));

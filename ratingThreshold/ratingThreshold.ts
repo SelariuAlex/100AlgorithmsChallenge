2eeb3fbf4aed5accd21a88653312c2f374ad1463
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-07-05 09:17:46 +0300

    rating threshold

--- ratingThreshold/ratingThreshold.ts
+++ ratingThreshold/ratingThreshold.ts
@@ -1,8 +1,18 @@
-function ratingThreshold(threshold: number, ratings: number[][]): number[] {
+function ratingThreshold(threshold, ratings) {
+  let review = [];
 
+  for (let i = 0; i < ratings.length; i++) {
+    let totalRating = 0;
+
+    ratings[i].forEach(rating => {
+      totalRating += rating;
+    });
+
+    if (totalRating / ratings[i].length < threshold) {
+      review.push(i);
+    }
+  }
+  return review;
 }
 
-console.log(ratingThreshold(3.5, 
-    [[3, 4],
-    [3, 3, 3, 4],
-    [4]]));
+console.log(ratingThreshold(3.5, [[3, 4], [3, 3, 3, 4], [4]]));

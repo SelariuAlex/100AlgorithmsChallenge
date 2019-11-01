4c53f54a7c192316dc010bc9d7609ecb89f6969a
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-11-01 17:56:03 +0200

    boxBlur

--- boxBlur/boxBlur.ts
+++ boxBlur/boxBlur.ts
@@ -1,3 +1,16 @@
-function boxBlur(image) {}
+function boxBlur(image) {
+  let allNumbersfromGrid = [];
+
+  image.forEach(array => {
+    array.forEach(number => {
+      allNumbersfromGrid.push(number);
+    });
+  });
+
+  const sumAllNumbers = allNumbersfromGrid.reduce((acc, num) => acc + num, 0);
+  const value = Math.floor(sumAllNumbers / allNumbersfromGrid.length);
+
+  return [value];
+}
 
 console.log(boxBlur([[1, 1, 1], [1, 7, 1], [1, 1, 1]]));

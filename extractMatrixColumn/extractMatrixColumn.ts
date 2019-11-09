98440826ff8455c675c50ba93de9e2dfc9ce09ee
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-11-09 16:49:39 +0200

    extract Matrix Column

--- extractMatrixColumn/extractMatrixColumn.ts
+++ extractMatrixColumn/extractMatrixColumn.ts
@@ -1,5 +1,11 @@
-function extractMatrixColumn(matrix: number[][], column: number): number[] {
+function extractMatrixColumn(matrix, column) {
+  let matrixColumnArr = [];
+  for (let i = 0; i < matrix.length; i++) {
+    matrixColumnArr.push(matrix[i][column]);
+  }
 
+  return matrixColumnArr;
 }
 
 console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
+console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 1));

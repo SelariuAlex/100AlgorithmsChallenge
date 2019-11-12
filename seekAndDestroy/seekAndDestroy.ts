6ad18dc340f023037cb268145e3443db17ba5509
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-11-12 14:32:05 +0200

    seek and destroy

--- seekAndDestroy/seekAndDestroy.ts
+++ seekAndDestroy/seekAndDestroy.ts
@@ -1,5 +1,5 @@
-function seekAndDestroy(arr1: number[], arr2: number[]): number[] {
-   
+function seekAndDestroy(arr1, arr2) {
+  return arr1.filter(e => !arr2.includes(e));
 }
 
 console.log(seekAndDestroy([3, 5, 1, 2, 2], [2, 3, 5]));

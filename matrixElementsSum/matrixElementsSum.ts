76e6a97cf9388418d54bc706a10895ee003b723d
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-12-15 16:13:13 +0200

    matrix sum

--- matrixElementsSum/matrixElementsSum.ts
+++ matrixElementsSum/matrixElementsSum.ts
@@ -1,9 +1,24 @@
-function matrixElementsSum(matrix: any[][]): number {
+function matrixElementsSum(matrix) {
+  let priceTotal = 0;
+  const bannedIndex = [];
 
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 0) {
+        bannedIndex.push(j);
+      } else if (bannedIndex.indexOf(j) === -1) {
+        priceTotal += matrix[i][j];
+      }
+    }
+  }
+
+  return priceTotal;
 }
 
-console.log(matrixElementsSum(
-    [[0, 1, 1, 2],
+console.log(
+  matrixElementsSum([
+    [0, 1, 1, 2],
     [0, 5, 0, 0],
-    [2, 0, 3, 3]]
-));
+    [2, 0, 3, 3]
+  ])
+);

3e3b7cd5ee0498a50ec2487db8c8b2521993a2c4
Selariu Alexandru <Selalex_gd@yahoo.com>
2019-12-22 10:54:18 +0200

    mincoins

--- minimalNumberOfCoins/minimalNumberOfCoins.ts
+++ minimalNumberOfCoins/minimalNumberOfCoins.ts
@@ -1,3 +1,16 @@
-function minimalNumberOfCoins(coins, price) {}
+function minimalNumberOfCoins(coins, price) {
+  let totalCoins = 0;
+
+  while (coins[coins.length - 1] <= price) {
+    let numCoins = Math.floor(price / coins[coins.length - 1]);
+    totalCoins += numCoins;
+    price = price - numCoins * coins[coins.length - 1];
+    coins.pop();
+  }
+
+  return totalCoins;
+}
 
 console.log(minimalNumberOfCoins([1, 2, 10], 28));
+console.log(minimalNumberOfCoins([1, 2, 10], 29));
+console.log(minimalNumberOfCoins([1, 2, 10], 39));
